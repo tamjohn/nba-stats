8da8598376dc8e695ee808f15b91279f6c328db1
tamjohn <tamjohn@student.ubc.ca>
2022-02-18 23:21:40 -0800

    LandingPage UI pt. 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
-import LandingPage from './component/landingPage'
+import React, {Component} from 'react';
+import axios from "axios";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
-  return(
-    <div className="App">
-      <header className="App-header">
-        <CssBaseline />
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-        </BrowserRouter>
-      </header>
-
-    </div>
-  )
+function createData(name, calories, fat, carbs, protein ) {
+  return { name, calories, fat, carbs, protein};
 }
 
+const rows = [
+  createData("Games Played:", 159, 6.0, 24, 4.0),
+  createData("Points Averaged:", 237, 9.0, 37, 4.3),
+  createData("Rebounds Averaged:", 262, 16.0, 24, 6.0),
+  createData("Assists Averaged", 305, 3.7, 67, 4.3),
+];
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      playerName: null,
+      playerStats: {}
+    }
+  }
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  this.getPlayerId()
+  console.log(this.state.playerName)
+}
+
+handleChange = (event) => {
+  const replace = event.target.value.split(" ").join("_");
+  if(replace.length > 0){
+    this.setState({playerName: replace})
+  } else {
+    alert("Please type players name!")
+  }
+}
+
+  getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+    .then(async res => {
+      // console.log(res.data.data)
+      if(res.data.data[0] === undefined){
+        alert("This player is either injured or hasn't played yet!")
+      } else if(res.data.data.length > 1){
+        alert("Pleases specify the name more!")
+      } else{
+        await this.getPlayerStats(res.data.data[0].id)
+
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=${playerId}`)
+    .then(async res => {
+      console.log(res.data.data)
+      this.setState({ playerStats: res.data.data[0]})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  
+  render(){
+  return (
+    <div className="App">
+    <img src='https://teamworxteambuilding.com/wp-content/uploads/2018/08/nba-logo-72dpi.jpg'
+    style={{
+        height: '80%',
+        width: '80%',
+        position: 'relative',
+        top: '-5px',
+        left: '10%'}}/>
+     <form onSubmit={this.handleSubmit}
+     style={{
+       position: 'relative',
+       left: '40%',
+       
+     }}>
+       <label>
+         Name
+         <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="please enter players name"
+         />
+       </label>
+       <input type="submit" value="Submit"/>
+     </form>
+     <div>
+       
+     <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+     </div>
+    </div>
+  );
+}
+}
 export default App;
--- src/component/landingPage.js
+++ src/component/landingPage.js
@@ -7,6 +7,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
+import { IconButton } from '@mui/material';
 
 function MyFormHelperText() {
   const { focused } = useFormControl() || {};
@@ -20,62 +21,21 @@ function MyFormHelperText() {
   }, [focused]);
 
   return <FormHelperText>{helperText}</FormHelperText>;
+
 }
 
-export default function UseFormControl() {
-  const [year, setYear] = React.useState('');
-  const [open, setOpen] = React.useState(false);
-
-  const handleChange = (event) => {
-    setYear(event.target.value);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
+export default function UseFormControl(){
   return (
-      <div>
+    <div>
     <img src='https://teamworxteambuilding.com/wp-content/uploads/2018/08/nba-logo-72dpi.jpg'
     style={{
-        height: '100%',
-        width: '100%',
+        height: '80%',
+        width: '80%',
         position: 'relative',
-        top: '-50px'}}/>
-        <div>
-        <Button sx={{ display: 'block', mt: 2 }} onClick={handleOpen}>
-      </Button>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-controlled-open-select-label">Year</InputLabel>
-        <Select
-          labelId="demo-controlled-open-select-label"
-          id="demo-controlled-open-select"
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={year}
-          label="Year"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>2020-2021</MenuItem>
-          <MenuItem value={20}>2019-2020</MenuItem>
-          <MenuItem value={30}>2018-2019</MenuItem>
-          <MenuItem value={30}>2017-2018</MenuItem>
-          <MenuItem value={30}>2016-2017</MenuItem>
-        </Select>
-      </FormControl>
-        </div>
-    <Box component="form" noValidate autoComplete="off">
-      <FormControl sx={{ width: '25ch'}}>
+        top: '-5px'}}/>
+    <Box component="form" noValidate autoComplete="off" >
+      <FormControl sx={{ width: '35ch'}}>
         <OutlinedInput placeholder="Enter NBA player's first and last name" />
-        <MyFormHelperText />
       </FormControl>
     </Box>
     </div>
